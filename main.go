8397349a8ec7f5fb2e428429eca8a51ed9de7cc5
Paul Kozlov <wert822@mail.ru>
2024-10-23 15:53:49 +0300

    Database installation

--- main.go
+++ main.go
@@ -8,32 +8,53 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var Message string
-
-type requestBody struct {
-	Message string `json:"message"`
+// Структура для JSON
+type InputData struct {
+	Text string `json:"text"`
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+
+	var message []Message
+	if err := DB.Find(&message).Error; err != nil {
+		http.Error(w, "Error retrieving records", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
 }
 
 func MessageHandler(w http.ResponseWriter, r *http.Request) {
-	var msg requestBody
-
-	err := json.NewDecoder(r.Body).Decode(&msg)
+	// Декодирование JSON
+	var inputData InputData
+	err := json.NewDecoder(r.Body).Decode(&inputData)
 	if err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		http.Error(w, fmt.Sprintf("Error decoding JSON: %v", err), http.StatusBadRequest)
 		return
 	}
 	defer r.Body.Close()
 
-	Message = msg.Message
+	// Создание новой записи в базе данных
+	message := Message{Text: inputData.Text}
+	if err := DB.Create(&message).Error; err != nil {
+		http.Error(w, fmt.Sprintf("Error saving message to database: %v", err), http.StatusInternalServerError)
+		return
+	}
 
-	fmt.Fprintf(w, "Hello, %s", Message)
+	// Успешный ответ
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "Message saved successfully with ID: %d", message.ID)
 }
 
 func main() {
+
+	// Вызываем метод InitDB() из файла db.go
+	InitDB()
+
+	// Автоматическая миграция модели Message
+	DB.AutoMigrate(&Message{})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
 	router.HandleFunc("/api/message", MessageHandler).Methods("POST")
